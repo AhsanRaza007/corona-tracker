f3744dc06ea407f0d012bfafcf41f33f3e4ba6bc
AhsanRaza007 <ahsan007raza@gmail.com>
2020-05-26 18:06:35 +0530

    Updates

---
+++ precache-manifest.d08bb15b9bce7adc092f6514139fb551.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8d6b67bcd6987fd080e7231d3ea4af",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "4c5f3d42f57ca41d50ba",
+    "url": "/corona-tracker/static/css/main.cdecf208.chunk.css"
+  },
+  {
+    "revision": "7ef30825528f66dffaf7",
+    "url": "/corona-tracker/static/js/2.4eb53f47.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.4eb53f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c5f3d42f57ca41d50ba",
+    "url": "/corona-tracker/static/js/main.2742ab92.chunk.js"
+  },
+  {
+    "revision": "7e7c08e4a6218267afe9",
+    "url": "/corona-tracker/static/js/runtime-main.1b83197d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
